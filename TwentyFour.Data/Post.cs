9ae4697a348696aef88c053affb0c027afcbb610
Raymond Sipes <raymondsipes21@gmail.com>
2021-06-01 15:48:51 -0400

    took away foreignkey in post

--- TwentyFour.Data/Post.cs
+++ TwentyFour.Data/Post.cs
@@ -15,11 +15,8 @@ namespace TwentyFour.Data
         public string Title { get; set; }
         public string Text { get; set; }
         public Guid AuthorId { get; set; }
-        public int CommentId { get; set; }
-        //[ForeignKey(nameof(CommentId))]
+
         public virtual List<Comment> Comments { get; set; }
-        public int LikeId { get; set; }
-        //[ForeignKey(nameof(LikeId))]
 
         public virtual List<Like> Likes { get; set; }
 
