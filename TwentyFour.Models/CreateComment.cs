d0283abc50ac92e18f6f96568df8bfbfd7c0f3f7
Chris Kronberg <ckronber@gmail.com>
2021-06-01 20:53:21 -0400

    GetallComments and CreateComment working

--- TwentyFour.Models/CreateComment.cs
+++ TwentyFour.Models/CreateComment.cs
@@ -14,13 +14,12 @@ namespace TwentyFour.Models
         [Required]
         public string Text { get; set; }
 
-
-        public int ReplyId { get; set; }
+        public int? ReplyId { get; set; }
 
         [ForeignKey(nameof(ReplyId))]
         public virtual List<Reply> Replies { get; set; }
 
-        public int PostId { get; set; }
+        public int? PostId { get; set; }
 
         [ForeignKey(nameof(PostId))]
         public virtual Post Post { get; set; }
--- TwentyFour.Models/GetAllComments.cs
+++ TwentyFour.Models/GetAllComments.cs
@@ -12,12 +12,12 @@ namespace TwentyFour.Models
     {
         public string Text { get; set; }
 
-        public int ReplyId { get; set; }
+        public int? ReplyId { get; set; }
 
         [ForeignKey(nameof(ReplyId))]
         public virtual List<Reply> Replies { get; set; }
 
-        public int PostId { get; set; }
+        public int? PostId { get; set; }
 
         [ForeignKey(nameof(PostId))]
         public virtual Post Post { get; set; }
