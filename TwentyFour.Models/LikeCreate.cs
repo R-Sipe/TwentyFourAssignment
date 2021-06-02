870facdb9408d56954b7e26c3665b3f304368b0d
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-02 09:45:09 -0400

    Andrew Commit to merge

---
+++ TwentyFour.Models/LikeCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwentyFour.Data;
+
+namespace TwentyFour.Models
+{
+    public class LikeCreate
+    {
+        public int PostId { get; set; }
+        [ForeignKey(nameof(PostId))]
+        public virtual Post Post { get; set; }
+    }
+}
